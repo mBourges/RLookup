3a9045a1868ac2da9408cf394ed947eb3bbc95bc
Matt <m.bourges@gmail.com>
2016-01-30 04:27:20 +0000

    chore(build tools): adding semantic-release, travisCI, mocha, chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+// var inputElement = require('./index')
+
+describe('lookup', function() {
+	it('should work', function() {
+		expect(true).to.be.true;
+	})
+});
